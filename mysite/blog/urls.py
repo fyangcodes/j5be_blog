1f3ce09f8544dbb8fea728549baf9887a230073f
Fengkai Yang <blesseven@gmail.com>
2025-01-27 21:42:46 +0100

    p156

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from . import views
+from . import views, feeds
 
 app_name = "blog"
 
@@ -14,4 +14,6 @@ urlpatterns = [
     ),
     path("<int:post_id>/share/", views.post_share, name="post_share"),
     path("<int:post_id>/comment/", views.post_comment, name="post_comment"),
+    path("feed/", feeds.LatestPostFeed(), name="post_feed"),
+    path("search/", views.post_search, name="post_search"),
 ]
